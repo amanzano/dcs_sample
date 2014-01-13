816bf772866bba8334db228330830e8826038e95
Audrey Manzano <amanzano@stratpoint.com>
2014-01-13 11:05:08 +0800

    Initial Commit

---
+++ iOSLecture/DCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DCSAppDelegate.h
+//  iOSLecture
+//
+//  Created by Audrey Manzano on 1/13/14.
+//  Copyright (c) 2014 Audrey Manzano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSLecture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSLecture
+//
+//  Created by Audrey Manzano on 1/13/14.
+//  Copyright (c) 2014 Audrey Manzano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DCSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DCSAppDelegate class]));
+    }
+}
